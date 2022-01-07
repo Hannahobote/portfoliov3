09e216cc0b4e4cd62ba432ce92a6a67e8539a178
Hannah Obote <ao223ir@student.lnu.se>
2022-01-07 20:03:33 +0100

    added contact info

--- src/components/Mynavbar.js
+++ src/components/Mynavbar.js
@@ -8,7 +8,7 @@ function Mynavbar() {
       <Link to="/" className="d-flex align-items-center text-dark text-decoration-none"><span class="fs-4">imhannaho</span></Link>
       <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto mx-2 mt-2">
         <Link class="me-3 py-2 text-dark text-decoration-none" to="/">Home</Link>
-        <Link class="me-3 py-2 text-dark text-decoration-none" to="/comingsoon">About</Link>
+        <Link class="me-3 py-2 text-dark text-decoration-none" to="/about">About</Link>
         <Link class="me-3 py-2 text-dark text-decoration-none" to="/comingsoon">Projects</Link>
       </nav>
     </div>
