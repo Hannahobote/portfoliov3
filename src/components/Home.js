fb0d08366bf2451c06075a12b712808d09474b95
Hannah Obote <ao223ir@student.lnu.se>
2022-01-07 19:44:34 +0100

    style chafe + about page update

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,13 +7,14 @@ function Home() {
       <div className='container-md'>
         <div className='row'>
           <section className='intro col mx-5 my-5 text-center'>
-            <h1>Hello I'm Hannah Obote,</h1>
-            <h1 className='mb-5'>an aspiring fullstack developer </h1>
-            <h4>I'm currently a computer science student at Linnaeus University, and will be graduation 2024.</h4>
-            <p>I'm currently learning how to build fullstack applications using JavaScript and it's various frameworks at univeristy. <br></br>
-              I'm also learning React.js and MERN-stack for a course im taking in March 2022. In the future I plan learn how to build  fullstack applications with Next.js, firebase, nextAuth, TailwindCSS and much more.
-            </p>
-            <Link to='/commingsoon' className='btn btn-outline-dark'>Ckeck out my projects</Link>
+            <header className='my-h-font'>
+              <h1>Hello I'm Hannah Obote,</h1>
+              <h1>an aspiring fullstack developer </h1>
+            </header>
+            <h5 className='my-h-font-bold'>I'm a computer science student at Linnaeus University, and will be graduation 2024.</h5>
+
+           
+            <Link to='/comingsoon' className='btn btn-outline-dark my-p-font'>Check out my projects</Link>
           </section>
         </div>
       </div>
--- src/components/Mynavbar.js
+++ src/components/Mynavbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Mynavbar() {
   return (
-    <div class="d-flex flex-column flex-md-row align-items-center pb-3 my-2 mx-2 border-bottom">
+    <div class="d-flex flex-column flex-md-row align-items-center pb-3 my-2 mx-2 my-h-font">
       <Link to="/" className="d-flex align-items-center text-dark text-decoration-none"><span class="fs-4">imhannaho</span></Link>
       <nav class="d-inline-flex mt-2 mt-md-0 ms-md-auto mx-2 mt-2">
         <Link class="me-3 py-2 text-dark text-decoration-none" to="/">Home</Link>
