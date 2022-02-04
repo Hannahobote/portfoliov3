49bd5679a9b46a4810b38cb8956793811ed00989
Hannah Obote <ao223ir@student.lnu.se>
2022-02-04 22:25:38 +0100

    change color theme to dark colors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom'
 
 function Footer() {
   return (
-    <footer class="text-gray-600 body-font">
+    <footer class="text-gray-400 bg-gray-900 body-font">
       <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <Link to="/" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
           </svg>
-          <span class="ml-3 text-xl">imhannaho</span>
+          <span class="ml-3 text-xl text-white">imhannaho</span>
         </Link>
-        <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2020 imhannaho</p>
+        <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2022 imhannaho</p>
         <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
 
           <a href="https://www.linkedin.com/in/hannah-obote-61110619a/" class="ml-3 text-gray-500">
--- src/components/MyProjectsCard.js
+++ src/components/MyProjectsCard.js
@@ -3,13 +3,14 @@ import React from 'react'
 function MyCard({ github, link, name, description }) {
   return (
     <div class="xl:w-1/4 md:w-1/2 p-4">
-    <div class="bg-gray-100 p-6 rounded-lg">
-      <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-      <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{link}</h3>
-      <h3 class="tracking-widest text-indigo-500 text-xs font-medium title-font">{github}</h3>
-      <h2 class="text-lg text-gray-900 font-medium title-font mb-4">{name}</h2>
-      <p class="leading-relaxed text-base">{description}</p>
-    </div>
+      <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+        <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
+        <h2 class="text-lg text-white font-medium title-font mb-4">{name}</h2>
+        <p class="leading-relaxed text-base">{description}</p>
+        <a href={github} class="text-indigo-400 inline-flex items-center mt-4 mx-3">Link to website</a>
+        <a href={link} class="text-indigo-400 inline-flex items-center mt-4">source code</a>
+
+      </div>
     </div>
   )
 }
