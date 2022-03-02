b6a4e90b4b1b303693ee5629e0eb91ebcd38245d
Hannah Obote <ao223ir@student.lnu.se>
2022-03-02 19:27:55 +0100

    spelling mistake fix

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,8 @@ function Home() {
     <section class="text-gray-400 bg-gray-900 body-font ">
       <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white"> Hello Im Hannah
-            <br class="hidden lg:inline-block" /> an inspiring fullstack developer
+          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-white"> Hello Im Hannah,
+            <br class="hidden lg:inline-block" /> an aspiring fullstack developer
           </h1>
           <p class="mb-8 leading-relaxed text-slate-400"> I'm currently an computer science student at Linnaeus University, and will graduate 2024.
            <br/> My university focuses mainly on teaching how to develop full stack applications. </p>
